b6089caa0fcbf48448a2553496b1830bc5fc4361
NeoLumo <venusresep@gmail.com>
2025-10-07 10:04:21 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,392 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7302987797690186,
+        "pitch": 0.10513372648760644,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.770196347530126,
+          "pitch": 0.07109438745734487,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Ресепш",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.16433522545924717,
+        "pitch": 0.002687200237025067,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8960614251918795,
+          "pitch": 0.09927309145817276,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.468128134615755,
+          "pitch": -0.047138174419114165,
+          "rotation": 3.141592653589793,
+          "target": "2--2"
+        },
+        {
+          "yaw": -0.18789320422479072,
+          "pitch": -0.035577712410365336,
+          "rotation": 4.71238898038469,
+          "target": "3-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--2",
+      "name": "Ресепшн 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.43455136478224965,
+        "pitch": 0.08009666256150183,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.84583648517639,
+          "pitch": 0.09193962911206022,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1-",
+      "name": "1 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.03522945513181952,
+        "pitch": 0.05756947621607722,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.91229797070606,
+          "pitch": 0.16519546129885398,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.012786294680703136,
+          "pitch": -0.016719347881110735,
+          "rotation": 4.71238898038469,
+          "target": "4--"
+        },
+        {
+          "yaw": 1.5213158625705638,
+          "pitch": 0.18787461814590856,
+          "rotation": 6.283185307179586,
+          "target": "6--"
+        },
+        {
+          "yaw": 3.0601499623250934,
+          "pitch": 0.16987801918599033,
+          "rotation": 4.71238898038469,
+          "target": "5--"
+        },
+        {
+          "yaw": 3.0612932886435527,
+          "pitch": 0.018130411231126686,
+          "rotation": 4.71238898038469,
+          "target": "7--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Процедурный кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.2822067258059491,
+        "pitch": 0.18429172893486978,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4391221953493023,
+          "pitch": 0.08698297063062022,
+          "rotation": 0,
+          "target": "3-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Кабинет ТМ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.8485883712703703,
+        "pitch": 0.09134089562473235,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.093274238874649,
+          "pitch": 0.24661586552372583,
+          "rotation": 3.141592653589793,
+          "target": "3-1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "Кабинет диагностики",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.7343657100898646,
+        "pitch": 0.10022780773452844,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.057234956962695,
+          "pitch": -0.06668301169341895,
+          "rotation": 0,
+          "target": "3-1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.33104891527521296,
+          "pitch": -0.13318539861960232,
+          "title": "Аппарат диагностики",
+          "text": "<strong data-start=\"69\" data-end=\"109\">7D-аппарат для диагностики кожи лица</strong> — профессиональное устройство, которое с высокой точностью анализирует состояние кожи в семи измерениях: выявляет морщины, поры, пигментацию, увлажнённость, жирность, чувствительность и возрастные изменения. Позволяет составить персональную программу ухода и отслеживать результаты процедур."
+        },
+        {
+          "yaw": 1.3223175405961705,
+          "pitch": -0.15940519986383705,
+          "title": "Аппарат диагностики",
+          "text": "<strong data-start=\"69\" data-end=\"109\">7D-аппарат для диагностики кожи лица</strong> — профессиональное устройство, которое с высокой точностью анализирует состояние кожи в семи измерениях: выявляет морщины, поры, пигментацию, увлажнённость, жирность, чувствительность и возрастные изменения. Позволяет составить персональную программу ухода и отслеживать результаты процедур."
+        }
+      ]
+    },
+    {
+      "id": "7--",
+      "name": "Кабинет косметологов",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.016655592656889,
+        "pitch": 0.1443023736747957,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4543682954580497,
+          "pitch": 0.0838521926363569,
+          "rotation": 12.566370614359176,
+          "target": "3-1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
